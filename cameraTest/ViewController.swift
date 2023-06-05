e0b65d6d9b68cec967d1998b704c32330d2b2222
InoueYuta-08 <y.inoue@icsoft.co.jp>
2023-06-05 15:08:06 +0900

    Initial Commit

---
+++ cameraTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cameraTest
+//
+//  Created by 井上雄太 on 2023/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
